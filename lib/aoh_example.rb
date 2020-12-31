106c07c0df9084ea2954abbb627e4afaad94801b
Jade McDaniels <jademcdaniels81@gmail.com>
2020-12-31 22:03:39 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -6,9 +6,9 @@ PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
 KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
-def assembled_aoh(results)
+def assembled_aoh
   
-  results = [DON_G = { name:  "Don Gately", occupation: "Live-in Staff" },
+[DON_G = { name:  "Don Gately", occupation: "Live-in Staff" },
 JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" },
 PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" },
 KATE_G = { name:  "Kate Gompert", occupation: "None" },
